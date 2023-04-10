5d88b11270aeaa5d6ac2cd479b17a411e2507046
Martin-EvansUWA <23621647@student.uwa.edu.au>
2023-04-10 20:41:30 +0800

    added password hash for user

--- src/app/main/models.py
+++ src/app/main/models.py
@@ -1,7 +1,7 @@
 from app import db
 from datetime import datetime
 from flask_login import UserMixin
-
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class Users(UserMixin, db.Model):
     # Primary key
@@ -18,6 +18,11 @@ class Users(UserMixin, db.Model):
     # Relationship
     messages = db.relationship('Messages', backref='author', lazy='dynamic')
 
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
     
     def __repr__(self):
         return '<User {}>'.format(self.username)
