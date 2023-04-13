0189f161d9e22daf863cb61425b7ccc8363b2eb4
Martin-EvansUWA <23621647@student.uwa.edu.au>
2023-04-13 11:48:25 +0800

    added login changes and some css adjust

--- src/app/main/routes.py
+++ src/app/main/routes.py
@@ -12,24 +12,18 @@ from app import db
 @main.route("/", methods=["GET", "POST"])
 @main.route("/index", methods=["GET", "POST"])
 def index():
-    # Login Popup
-    login_form = LoginForm()
+    return render_template("index.html")
+
+
+@main.route("/about")
+def about():
+    return render_template("about_us.html")
+
+# To logout user
+@main.route("/signup")
+def signup():
     register_form = RegisterForm()
-
-    if request.method == "POST":
-        if login_form.validate_on_submit():
-            # parse login information
-            flash(
-                "Logged in: "
-                + login_form.username.data
-                + ", "
-                + login_form.password.data
-            )
-            login(login_form)
-            return redirect(url_for(".index"))  # Redirect to clear POST data
-
-        # Register Popup
-        if register_form.validate_on_submit():
+    if register_form.validate_on_submit():
             # parse registration information
             flash(
                 "Logged in: "
@@ -37,42 +31,35 @@ def index():
                 + ", "
                 + register_form.password.data
             )
-            if not current_user.is_authenticated:
-                signup(register_form)
                 # Login user
-                login(register_form)
-                return redirect(url_for(".index"))  # Redirect to clear POST data
-    return render_template("index.html", login=login_form, register=register_form)
+            user = Users(username=register_form.username.data, email=register_form.email.data)
+            user.set_password(register_form.password.data)
 
-
-@main.route("/about")
-def about():
-    return render_template("about_us.html")
-
-
-# To logout user
-@main.route("/signup")
-def signup(register_form: RegisterForm):
-    user = Users(username=register_form.username.data, email=register_form.email.data)
-    user.set_password(register_form.password.data)
-
-    db.session.add(user)
-    db.session.commit()
-    return redirect(url_for(".index"))
+            db.session.add(user)
+            db.session.commit()
+            return redirect(url_for(".login"))
+    return render_template("signup.html", form=register_form)
 
 
 # To logout user
-@main.route("/login")
-def login(login_form: FlaskForm):
-    user = Users.query.filter_by(username=login_form.username.data).first()
-    if user is None or not user.check_password(login_form.password.data):
-        flash("Invalid username or password")
-        return redirect(url_for(".index"))
-    login_user(user, remember=login_form.remember_me.data)
-    next_page = request.args.get("next")
-    if not next_page or url_parse(next_page).netloc != "":
-        next_page = url_for(".index")
-    return redirect(next_page)
+@main.route("/login", methods=["GET", "POST"])
+def login():
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        # parse login information
+        flash(
+            "Logged in: " + login_form.username.data + ", " + login_form.password.data
+        )
+        user = Users.query.filter_by(username=login_form.username.data).first()
+        if user is None or not user.check_password(login_form.password.data):
+            flash("Invalid username or password")
+            return redirect(url_for(".index"))
+        login_user(user, remember=login_form.remember_me.data)
+        next_page = request.args.get("next")
+        if not next_page or url_parse(next_page).netloc != "":
+            next_page = url_for(".index")
+        return redirect(next_page)
+    return render_template("login.html", form=login_form)
 
 
 # To logout user
