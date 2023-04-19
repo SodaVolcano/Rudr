fcb4ed0cfe7bfe9dec875ed9f36a5ea8b0cda9f0
23348918 <23348918@student.uwa.edu.au>
2023-04-19 13:22:38 +0800

    added hamburger menu on smaller screens

---
+++ src/app/static/base.js
@@ -0,0 +1,9 @@
+/* Open the overlay when someone clicks on the hamburger menu */
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+
+/* Close the overlay when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
