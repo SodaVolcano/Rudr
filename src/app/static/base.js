3ec937189b4868b77546a8a6548a7492d2605599
SodaVolcano <tin1771718335@gmail.com>
2023-04-24 18:20:23 +0800

    cleaned up HTML - still need to work on sign-in/login HTML

--- src/app/static/base.js
+++ src/app/static/base.js
@@ -1,9 +1,9 @@
+"use strict";
 /* Open the overlay when someone clicks on the hamburger menu */
-function openNav() {
-  document.getElementById("myNav").style.width = "100%";
-}
-
+$('.hamburger-menu')[0].addEventListener('click', function (event) {
+    $('#overlay-nav')[0].style.width = "100%";
+});
 /* Close the overlay when someone clicks on the "x" symbol inside the overlay */
-function closeNav() {
-  document.getElementById("myNav").style.width = "0%";
-}
+$('.closebtn')[0].addEventListener('click', function (event) {
+    $('#overlay-nav')[0].style.width = "0%";
+});
