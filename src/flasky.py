e08ed229d18e35ad26468ed198de1600a429d2c1
SodaVolcano <tin1771718335@gmail.com>
2023-05-21 21:52:18 +0800

    idk

--- src/flasky.py
+++ src/flasky.py
@@ -1,7 +1,30 @@
 """ Script to run in flask shell """
 import os
+import subprocess
 
 from app import create_app, db
+
+
+def run_command(command):
+    """Initlaise the database and run the migrations"""
+    process = subprocess.Popen(
+        command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+    )
+    stdout, stderr = process.communicate()
+
+    if process.returncode != 0:
+        # An error occurred
+        print(f"Command failed with error code {process.returncode}:")
+        print(stderr.decode())
+    else:
+        # The command was successful
+        print(stdout.decode())
+
+
+command = (
+    "cd ./src/ && flask db init ; flask db migrate -m 'db init' ; flask db upgrade"
+)
+run_command(command)
 
 app = create_app(os.getenv("FLASK_CONFIG") or "default")
 
