fc962e5bbd27409b9e28a1e65b104e5e0eff1aa6
Martin Evans Laptop <martin@endevea.com>
2023-05-24 11:02:36 +0800

    not suspicuous commit

--- src/tests/dbtest.py
+++ src/tests/dbtest.py
@@ -1,3 +1,9 @@
+import sys
+import os
+
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.dirname(SCRIPT_DIR))
+
 import unittest
 from app import create_app, db
 from app.main.models import Users, Robot, Conversations, Messages
